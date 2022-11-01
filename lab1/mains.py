6494225dd40f91cf94fb010ada8f1ad1da0dccce
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2022-11-01 22:03:44 +0300

    Add files via upload

---
+++ lab1/mains.py
@@ -0,0 +1,13 @@
+from Cat import Cat
+from Dog import Dog
+from Snake import Snake
+ 
+def main():
+    #using polymorphism to get sounds from different animals
+    animals = [Cat(), Dog(), Snake()]
+    for animal in animals:
+        animal.talk()
+        animal.sound()
+
+
+main()
