835d09b4ede1137d715360c3ee64048afbfde9f0
Muganga_Charles <104686307+muganga-charles@users.noreply.github.com>
2023-03-02 22:22:00 +0300

    Update mains.py

--- lab1/mains.py
+++ lab1/mains.py
@@ -1,7 +1,7 @@
 from Cat import Cat
 from Dog import Dog
 from Snake import Snake
- #
+ 
 def main():
     #using polymorphism to get sounds from different animals
     animals = [Cat(), Dog(), Snake()]
